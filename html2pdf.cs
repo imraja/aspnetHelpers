d15b23ec7f4ae92a493a400d40d13cd68e1b1b2c
Rajasekar Sa <imraja@users.noreply.github.com>
2014-11-12 16:45:50 +0530

    Create html2pdf.cs

---
+++ html2pdf.cs
@@ -0,0 +1,18 @@
+HttpResponse response = context.Response;
+string filename = context.Request.QueryString["filename"];
+
+response.Clear();
+response.ContentType = "application/pdf";
+response.AddHeader("content-disposition", "attachment;filename=" + sFileName);
+response.Cache.SetCacheability(HttpCacheability.NoCache);
+
+System.IO.StringReader sr = new System.IO.StringReader(sHtml.Replace("../../Images/Cancel.gif", context.Server.MapPath("/Images/Cancel.gif")));
+iTextSharp.text.Document pdfDoc = new iTextSharp.text.Document(iTextSharp.text.PageSize.A4, 20f, 20f, 50f, 30f);
+
+PdfWriter writer = PdfWriter.GetInstance(pdfDoc, response.OutputStream);
+pdfDoc.Open();
+XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+pdfDoc.Close();
+
+response.Write(pdfDoc);
+response.End();
